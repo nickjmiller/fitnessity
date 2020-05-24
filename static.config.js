ddfdc639172b7842884524748ea7eb6607841d82
Nick Miller <nickamilli@gmail.com>
2020-05-23 18:04:39 -0600

    Add title to documents

--- static.config.js
+++ static.config.js
@@ -1,10 +1,26 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable indent */
+import React from "react";
 import path from "path";
 import EXERCISES from "./src/data/exercises";
-// import { Exercise } from "./types";
-// Typescript support in static.config.js is not yet supported, but is coming in a future update!
 
 export default {
     entry: path.join(__dirname, "src", "index.tsx"),
+    Document: ({
+        Html,
+        Head,
+        Body,
+        children,
+    }) => (
+            <Html lang="en-US">
+                <Head>
+                    <title>Integrum</title>
+                    <meta charSet="UTF-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                </Head>
+                <Body>{children}</Body>
+            </Html>
+        ),
     getRoutes: async () => [
         {
             path: "/exercises",
@@ -20,6 +36,7 @@ export default {
             })),
         },
     ],
+    siteroot: "https://integrum.nickmiller.dev",
     plugins: [
         "react-static-plugin-typescript",
         [
