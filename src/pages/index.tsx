580f9816080e82e714eb0debafdd128e16afe22c
Nick Miller <nickamilli@gmail.com>
2020-06-13 13:13:08 -0600

    Minor UX tweaks for workout customizations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import WorkoutContainer from "components/workout/WorkoutContainer";
-import { Button, Flex, Text, Box } from "rebass";
+import {
+    Button, Flex, Text, Box,
+} from "rebass";
 import { WORKOUT_MAP, WorkoutGroup } from "../data/MuscleMap";
 // eslint-disable-next-line import/named
 import EXERCISES, { Exercise } from "../data/exercises";
@@ -41,22 +43,28 @@ export default () => {
             <Box hidden={hidden}>
                 <Text py={2} fontSize={[12, 14, 16]}>
                     Customize workout (This will erase current progress)
-                    {hidden}
                 </Text>
                 <Flex flexWrap="wrap" justifyContent="space-around" maxWidth={768} margin="auto">
-                    <Button variant="Primary" onClick={() => setWorkoutPlan(shuffleExercises(["Upper", "Upper", "Upper", "Upper", "Core"]))}>
+                    <Button onClick={() => setWorkoutPlan(shuffleExercises(["Upper", "Upper", "Upper", "Upper", "Core"]))}>
                         Upper!
                     </Button>
-                    <Button variant="Primary" onClick={() => setWorkoutPlan(shuffleExercises(["Lower", "Lower", "Lower", "Lower", "Core"]))}>
+                    <Button onClick={() => setWorkoutPlan(shuffleExercises(["Lower", "Lower", "Lower", "Lower", "Core"]))}>
                         Lower!
                     </Button>
-                    <Button variant="Primary" onClick={() => setWorkoutPlan(shuffleExercises([...DEFAULT_WORKOUT_PLAN]))}>
+                    <Button
+                        onClick={() => setWorkoutPlan(shuffleExercises([...DEFAULT_WORKOUT_PLAN]))}
+                    >
                         Balanced!
                     </Button>
-                    <Button onClick={() => setHidden(true)}>
+                    <Button variant="outline" onClick={() => setHidden(true)}>
                         Hide
                     </Button>
                 </Flex>
+            </Box>
+            <Box hidden={!hidden}>
+                <Button variant="outline" onClick={() => setHidden(false)}>
+                    Show Workout Options
+                </Button>
             </Box>
         </>
     );
