da086684a182f7672516a47863315416f12d755b
Nick Miller <nickamilli@gmail.com>
2020-09-04 18:27:35 -0600

    Fix leaderboard button name

--- src/components/LeaderboardModal.tsx
+++ src/components/LeaderboardModal.tsx
@@ -37,7 +37,7 @@ export default () => {
     return (
         <Box>
             <Button ml={2} onClick={openModal}>
-                Workout
+                Leaderboard
             </Button>
             <Modal
                 isOpen={modalIsOpen}
